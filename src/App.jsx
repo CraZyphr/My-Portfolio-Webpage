049631ce3c9e4077e857ee98e5bbe5ccc3ac9889
Zyphr <142991851+CraZyphr@users.noreply.github.com>
2023-11-07 22:30:37 +0530

    Made transition animation and Skills page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import'./app.scss'
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Parallax from './components/parallax/Parallax';
+import Whatiknow from './components/what i know/Whatiknow';
 
 const App = () => {
   return <div>
@@ -10,7 +11,7 @@ const App = () => {
       <Hero/>
       </section>
     <section id='Projects'><Parallax type="what i know"/></section>
-    <section id='What I Know?'>Portfolio1</section>
+    <section id='What I Know?'><Whatiknow/></section>
     <section id='Portfolio'><Parallax type="portfolio"/></section>
     <section id='Contact'>Contact</section>
     <section id='About'>Portfolio3</section>
